e5b791424ae0b2890e5a6a5866c3b104ebf16803
Oliver Nybo <olivernybo@pcm60140.eadania.net.local>
2020-03-06 11:23:16 +0100

    comments

--- LinkedText.lua
+++ LinkedText.lua
@@ -1,18 +1,24 @@
+-- https://riptutorial.com/lua/example/20315/lua-pattern-matching luas pattern matching.
 URL_PATTERNS = { 
-    ".+%..+/.*",
-    ".+%..+"
+    ".+%..+/.*", -- url's with slash
+    ".+%..+" -- url's without slash
 }
 
+-- no clue about this, Private explain
 function formatURL(url)
     url = "|cff".."149bfd".."|Hurl:"..url.."|h["..url.."]|h|r ";
     return url;
 end
 
 function makeClickable(self, event, msg, ...)
-    local newmsg = ""
+	local newmsg = ""
+	-- loop through every word in msg
     for word in msg:gmatch("%S+") do
-        local foundLink = false
-        for k,p in pairs(URL_PATTERNS) do
+		local foundLink = false
+		-- loop through all url patterns
+		for k,p in pairs(URL_PATTERNS) do
+			-- if the word is an url and we haven't found an url in the word yet
+			-- then format the word
             if string.find(word, p) and foundLink == false then
                 word = string.gsub(word, p, formatURL("%1"))
                 foundLink = true
@@ -24,11 +30,11 @@ function makeClickable(self, event, msg, ...)
     return false, msg, ...
 end
 
+-- popup style
 StaticPopupDialogs["CLICK_LINK_CLICKURL"] = {
-    text = "Copy & Paste the link into your browser",
+    text = "Copy the link",
     button1 = "Close",
-    OnAccept = function()
-    end,
+    OnAccept = function() end,
     timeout = 0,
     whileDead = true,
     hideOnEscape = true,
@@ -41,6 +47,7 @@ StaticPopupDialogs["CLICK_LINK_CLICKURL"] = {
     hasEditBox = true
 }
 
+-- Private explain
 local SetItemRef_orig = SetItemRef;
 function ClickURL_SetItemRef(link, text, button)
     if (string.sub(link, 1, 3) == "url") then
@@ -54,6 +61,7 @@ function ClickURL_SetItemRef(link, text, button)
 end
 SetItemRef = ClickURL_SetItemRef;
 
+-- chat types
 local CHAT_TYPES = {
     "AFK",
     "BATTLEGROUND_LEADER",
@@ -78,7 +86,7 @@ local CHAT_TYPES = {
     "SYSTEM"
 }
 
+-- loop chat types and add event
 for _, type in pairs(CHAT_TYPES) do
     ChatFrame_AddMessageEventFilter("CHAT_MSG_" .. type, makeClickable)
 end
--- https://riptutorial.com/lua/example/20315/lua-pattern-matching luas pattern matching.
